fbc8287e4e73d05390abf88534b054f6916bb2ad
CamilaRodriguez-web <83306932+CamilaRodriguez-web@users.noreply.github.com>
2022-06-12 23:37:22 -0300

    Agregamos mock de tareas e interfas de las mismas

---
+++ src/app/Task.ts
@@ -0,0 +1,6 @@
+export interface Task{
+    id: number; //podría no venir cuando creamos
+    text: string;
+    day: string;
+    reminder: boolean;
+}
---
+++ src/app/mock-task.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] =[
+    {
+        id: 1,
+        text: "Terminar proyecto Integrador",
+        day: 'Junio 25, 12:00hs',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer compras para Nicolas",
+        day: 'Junio 25, 14:30hs',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Cenar con Amigos",
+        day: 'Junio 25, 20:00hs',
+        reminder: true
+    },
+    {
+        id: 4,
+        text: "Investigar SQL",
+        day: 'Junio 25, 23:00hs',
+        reminder: false
+    },
+]
